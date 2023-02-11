1ab4e624ccb0b36818915c56d296cc1311c83385
SergeyFM <github@svistunov.su>
2023-02-11 18:27:39 +0300

    Initial commit

---
+++ src/main/java/com/example/springdemo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hallo")
+    public String helloWorld() {
+        return "Hello World! <br> ...by Sergey Svistunov";
+    }
+}
