2faa75ce059cf3936fc28bebd5025edb710919d9
SergeyFM <github@svistunov.su>
2023-02-12 12:32:18 +0300

    Investigating basic dependency injection workings

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -2,12 +2,28 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
+	public interface SaySomethingToSergeyService {
+		public String sayIt();
+	}
+
+	@Component
+	public class SayHiToSergeyService implements SaySomethingToSergeyService {
+		@Override
+		public String sayIt() {
+			return "Hi, Sergey!!";
+		}
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
+
+		ConfigurableApplicationContext appContext = SpringApplication.run(SpringDemoApplication.class, args);
+		SaySomethingToSergeyService saySomethingToSergeyService = appContext.getBean(SaySomethingToSergeyService.class);
+		System.out.println(saySomethingToSergeyService.sayIt());
 	}
 
 }
